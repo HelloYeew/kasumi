201ac1b62d478257654e2033de3a2bebb6de6461
Phawit Pornwattanakul <me@helloyeew.dev>
2022-01-06 03:03:58 +0700

    Change ping command to return as embed

--- main.rb
+++ main.rb
@@ -6,7 +6,11 @@ require 'dotenv/load'
 require 'json'
 
 bot_config = JSON.parse(File.read('./version.json'))
+COLOR = 14_584_191
+FOOTER_MESSAGE = "Kasumi #{bot_config['version']}"
+FOOTER_ICON = bot_config['icon_url']
 
+# Initialize the bot
 bot = Discordrb::Commands::CommandBot.new token: ENV['token'], prefix: ENV['prefix']
 
 # Initialize the logger
@@ -39,7 +43,12 @@ bot.command :ping do |event|
   # Return the time that the bot use to response
   logger.debug("Message received from '#{event.author.username}' in server '#{event.server.name}' (##{event.channel.name}) : #{event.message}")
   logger.debug("Responded by ping command with response time #{Time.now - event.timestamp} seconds")
-  "Pong! Time taken: #{Time.now - event.timestamp} seconds."
+  event.channel.send_embed do |embed|
+    embed.colour = COLOR
+    embed.title = 'Pong!'
+    embed.description = "Time taken : #{((Time.now - event.timestamp)*1000).to_i} ms"
+    embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: FOOTER_MESSAGE, icon_url: FOOTER_ICON)
+  end
 end
 
 bot.command :user do |event|
@@ -47,7 +56,7 @@ bot.command :user do |event|
   logger.debug("Message received from '#{event.author.username}' in server '#{event.server.name}' (##{event.channel.name}) : #{event.message}")
   logger.debug("Responded by user command with response time #{Time.now - event.timestamp} seconds")
   event.channel.send_embed do |embed|
-    embed.colour = 14_584_191
+    embed.colour = COLOR
     embed.title = "#{event.user.name}'s Profile"
     embed.description = 'Your full profile detail is here!'
     embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url.to_s)
@@ -56,7 +65,7 @@ bot.command :user do |event|
     embed.add_field(name: 'Bot?', value: event.user.current_bot?.to_s)
     embed.add_field(name: 'Status', value: event.user.status.to_s)
     embed.add_field(name: 'Playing', value: event.user.game) unless event.user.game.nil?
-    embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Kasumi #{bot_config['version']}", icon_url: bot_config['icon_url'])
+    embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: FOOTER_MESSAGE, icon_url: FOOTER_ICON)
   end
 end
 
